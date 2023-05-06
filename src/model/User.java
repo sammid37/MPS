90889a7bdab3ff021702756d70847709809a85b0
Maria Victória Grisi <maria.grisi@academico.ufpb.br>
2023-05-06 11:29:02 -0300

    Classe User atualizado com construtor, getters e setters

--- src/model/User.java
+++ src/model/User.java
@@ -1,9 +1,34 @@
 package model;
 
 public class User {
-  private String nome;
-  private String endereco;
-  private String telefone;
+  private int id;
+  private String login;
+  private String password;
 
   // construtor, getters e setters para os atributos acima
+  void public User(int id, String login, String password){
+    this.id = id;
+    this.login = login;
+    this.password = password;
+  }
+
+  String public getLogin(){
+    return this.login;
+  }
+
+  void public setLogin(String newLogin){
+    this.login = newLogin;
+  }
+
+  String public getPassword(){
+    return this.password;
+  }
+
+  void public setPassword(String newPassword){
+    this.password = newPassword;
+  }
+
+  int public getId(){
+    return this.id;
+  }
 }
