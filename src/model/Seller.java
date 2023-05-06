24095d15baf86a0d8711d59455f586c7674b34c0
Maria Victória Grisi <maria.grisi@academico.ufpb.br>
2023-05-06 11:30:04 -0300

    Classe Seller atualizada com construtor, getters, setters e adição e remoção na lista de produtos

--- src/model/Seller.java
+++ src/model/Seller.java
@@ -3,9 +3,40 @@ package model;
 import java.util.List;
 
 public class Seller extends User {
+  private String shopName;
   private String cnpj;
-  private List<Product> products;
+  private LinkedList<Product> products;
 
   // construtor, getters e setters para os atributos acima
   // métodos para adicionar e remover produtos
+  void public Seller(String shopName, String cnpj, int id, int login, int password){
+    super(id, login, password);
+    this.shopName = shopName;
+    this.cpnj = cpnj;
+    this.products = new LinkedList<Product>();
+  }
+
+  String public getShopName(){
+    return this.shopName;
+  }
+
+  void public setShopName(String newShopName){
+    this.shopName = newShopName;
+  }
+
+  String public getCnpj(){
+    return this.cnpj;
+  }
+
+  void public setCnpj(String newCnpj){
+    this.cnpj = newCnpj;
+  }
+
+  void public addProduct(String productName){
+    this.products.add(productName);
+  }
+
+  void public removeProduct(String productName){
+    this.products.remove(productName)
+  }
 }
