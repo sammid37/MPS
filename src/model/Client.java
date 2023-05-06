08bc395faba78f1597456149751049e839750aca
Maria Victória Grisi <maria.grisi@academico.ufpb.br>
2023-05-06 11:31:18 -0300

    Classe Client atualizado com construtor, getters, setters e adição e remoção de itens na lista de pedidos

--- src/model/Client.java
+++ src/model/Client.java
@@ -4,9 +4,40 @@ import java.util.List;
 
 public class Client extends User {
   private String cpf;
-  private List<Order> orders;
+  private LinkedList<Order> orders;
 
   // construtor, getters e setters para os atributos acima
   // métodos para adicionar e remover pedidos
-} 
+  void public Seller(String shopName, String cpf, int id, int login, int password){
+    super(id, login, password);
+    this.shopName = shopName;
+    this.cpnj = cpnj;
+  
+    this.orders = new LinkedList<Order>();
+  }
+
+  String public getShopName(){
+    return this.shopName;
+  }
+
+  void public setShopName(String newShopName){
+    this.shopName = newShopName;
+  }
+
+  String public getCpf(){
+    return this.cpf;
+  }
+
+  void public setCpf(String newCpf){
+    this.cpf = newCpf;
+  }
+
+  void public addOrder(String orderName){
+    this.orders.add(orderName);
+  }
+
+  void public removeOrder(String orderName){
+    this.orders.remove(orderName)
+  }
+}
   
